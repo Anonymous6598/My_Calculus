3ff067bb85e5406d2167b1181b4751359846fcbd
Виктор Илић <vikiilitch@outlook.com>
2025-03-23 16:29:48 +0100

    Add files via upload

---
+++ My_Calculus/windows/My_Calculus_settings_window_interface.py
@@ -0,0 +1,20 @@
+import abc
+
+class My_Calculus_settings_window_interface(abc.ABC):
+     
+     @abc.abstractmethod
+     def __change_text_color__() -> None:
+        pass
+     
+     @abc.abstractmethod
+     def __change_expression_entry_color__() -> None:
+        pass
+     
+     @abc.abstractmethod
+     def __change_expression_entry_text_color__() -> None:
+        pass
+     
+     @abc.abstractmethod
+     def __change_button_color__() -> None:
+        pass
+          
