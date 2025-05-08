2224ee3dcf74c6ec4cdde86597c689a63c2b25b7
Виктор Илић <vikiilitch@outlook.com>
2025-05-08 12:24:43 +0200

    Delete My_Calculus directory

--- My_Calculus/Linux/My_Calculus_AI_window_interface.py
+++
@@ -1,7 +0,0 @@
-import abc, typing
-
-class My_Calculus_AI_window_interface(abc.ABC):
-    
-    @abc.abstractmethod
-    def __response_from_ai__(self: typing.Self, configure: str | None = None) -> None:
-        pass
--- My_Calculus/windows/My_Calculus_AI_interface.py
+++
@@ -1,7 +0,0 @@
-import abc, typing
-
-class My_Calculus_AI_interface(abc.ABC):
-    
-     @abc.abstractmethod
-     def __response__(self: typing.Self, prompt: str) -> str:
-        pass
