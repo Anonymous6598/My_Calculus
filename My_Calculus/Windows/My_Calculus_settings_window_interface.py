c004b826e83b2373b939cdadd71a33f174c0e362
Viktor Ilić <vikiilitch@outlook.com>
2025-10-25 19:33:59 +0200

    Remove unnecessary whitespace from settings interface

--- My_Calculus/Windows/My_Calculus_settings_window_interface.py
+++ My_Calculus/Windows/My_Calculus_settings_window_interface.py
@@ -4,16 +4,16 @@ class My_Calculus_settings_window_interface(abc.ABC):
      
      @abc.abstractmethod
      def __change_text_color__(self: typing.Self, pickle_serializer: pickle) -> None:
-        pass
+         pass
      
      @abc.abstractmethod
      def __change_expression_entry_color__(self: typing.Self, pickle_serializer: pickle) -> None:
-        pass
+         pass
      
      @abc.abstractmethod
      def __change_expression_entry_text_color__(self: typing.Self, pickle_serializer: pickle) -> None:
-        pass
+         pass
      
      @abc.abstractmethod
      def __change_button_color__(self: typing.Self, pickle_serializer: pickle) -> None:
-        pass
+         pass
