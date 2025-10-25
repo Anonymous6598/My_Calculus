3a00d77e97d46603befc30340fe3fd0172fd9244
Viktor Ilić <vikiilitch@outlook.com>
2025-10-25 19:31:50 +0200

    Update My_Calculus_settings_window_interface.py

--- My_Calculus/Windows/My_Calculus_settings_window_interface.py
+++ My_Calculus/Windows/My_Calculus_settings_window_interface.py
@@ -1,20 +1,21 @@
-import abc
+import abc, typing
 
 class My_Calculus_settings_window_interface(abc.ABC):
      
      @abc.abstractmethod
-     def __change_text_color__() -> None:
+     def __change_text_color__(self: typing.Self, pickle_serializer: pickle) -> None:
         pass
      
      @abc.abstractmethod
-     def __change_expression_entry_color__() -> None:
+     def __change_expression_entry_color__(self: typing.Self, pickle_serializer: pickle) -> None:
         pass
      
      @abc.abstractmethod
-     def __change_expression_entry_text_color__() -> None:
+     def __change_expression_entry_text_color__(self: typing.Self, pickle_serializer: pickle) -> None:
         pass
      
      @abc.abstractmethod
-     def __change_button_color__() -> None:
+     def __change_button_color__(self: typing.Self, pickle_serializer: pickle) -> None:
         pass
+
           
