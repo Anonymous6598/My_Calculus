ecae2697436c68dcbe4f08ce0bcbd6c3a2fa67f3
Viktor Ilić <vikiilitch@outlook.com>
2025-10-25 19:39:33 +0200

    Update My_Calculus_AI_interface.py

--- My_Calculus/Copliot+PC/My_Calculus_AI_interface.py
+++ My_Calculus/Copliot+PC/My_Calculus_AI_interface.py
@@ -2,10 +2,11 @@ import abc, typing, openvino_genai
 
 class My_Calculus_AI_interface(abc.ABC):
     
-     @abc.abstractmethod
+    @abc.abstractmethod
     def __initialize_model__(self: typing.Self) -> openvino_genai.LLMPipeline:
         pass
     
     @abc.abstractmethod
     def __response__(self: typing.Self, prompt: str) -> str:
         pass
+
